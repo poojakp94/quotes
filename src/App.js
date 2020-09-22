54c90680e9904f4e36c52667886d1c489fe77653
poojakp94 <poojakprasad94@gmail.com>
2020-09-22 17:22:38 +0530

    added gh-pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import twitter from "./twitter.png";
+import "./App.css";
 
 function App() {
+  const [newQuote, setNewQuote] = useState({
+    quote: 'Part of what makes programming difficult is most of the time we’re doing stuff we’ve never done before.',
+    author: 'Douglas Crockford'
+  });
+
+  const getQuote = () => {
+    fetch("https://programming-quotes-api.herokuapp.com/quotes/random")
+    .then((response) => response.json())
+    .then((data) => {
+      setNewQuote({quote : data.en,
+      author : data.author});
+      
+    });
+  }
+  useEffect(() => {
+    getQuote();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="quote-box">
+        <div>
+          <p id="text">{newQuote.quote}</p>
+          <div id="author">- {newQuote.author}</div>
+        </div>
+        <div className="icon-wrapper">
+          <a href={`https://twitter.com/intent/tweet?text=${newQuote.quote} -${newQuote.author}`} id="tweet-quote" target="_blank">
+            <img src={twitter} alt=""></img>
+          </a>
+          <div>
+            <button id="new-quote" onClick={getQuote}>Next Quote</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
