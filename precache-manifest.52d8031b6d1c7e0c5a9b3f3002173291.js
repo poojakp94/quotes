9b922b279c0653d8e9da4175e08cd485948d8126
poojakp94 <poojakprasad94@gmail.com>
2020-09-22 17:23:51 +0530

    Updates

---
+++ precache-manifest.52d8031b6d1c7e0c5a9b3f3002173291.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78684044e7180305dd6eb6879d94c3ce",
+    "url": "/quotes/index.html"
+  },
+  {
+    "revision": "06d967d43c5e36935b73",
+    "url": "/quotes/static/css/main.3c142604.chunk.css"
+  },
+  {
+    "revision": "8aa7de0aa1d5a584ab18",
+    "url": "/quotes/static/js/2.2782b63c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quotes/static/js/2.2782b63c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06d967d43c5e36935b73",
+    "url": "/quotes/static/js/main.dd32e49e.chunk.js"
+  },
+  {
+    "revision": "42135f8f194da03e2725",
+    "url": "/quotes/static/js/runtime-main.8b2ab2e9.js"
+  },
+  {
+    "revision": "0251d8ee95aa6d1f3400faa3b46b4bcf",
+    "url": "/quotes/static/media/twitter.0251d8ee.png"
+  }
+]);
